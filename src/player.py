bdfcf0ef8130de56ecea4069da0943f219670862
Kauri Kolehmainen <kaurikolehmainen@gmail.com>
2023-09-15 14:05:17 +0300

    divided movement to separate functions

--- src/player.py
+++ src/player.py
@@ -16,11 +16,20 @@ class Player(pygame.sprite.Sprite):
     def events(self):
         buttons = pygame.key.get_pressed()
         if buttons[pygame.K_RIGHT] and self.rect.right <= self.width:
-            self.rect.x += self.velocity
+            self.move_right(self.velocity)
         if buttons[pygame.K_LEFT] and self.rect.left >= 0:
-            self.rect.x -= self.velocity
+            self.move_left(self.velocity)
         if buttons[pygame.K_SPACE] and self.rect.bottom >= self.height-int(10*6.4):
-            self.gravity = -20
+            self.jump()
+
+    def move_right(self, velocity: int):
+        self.rect.x += velocity
+
+    def move_left(self, velocity: int):
+        self.rect.x -= velocity
+
+    def jump(self):
+        self.gravity = -20
 
     def fall(self):
         self.gravity += 1
